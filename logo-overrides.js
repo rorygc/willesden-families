876e7a89ee60cd0c85c05f508042ed54290de004
Rory Cooney <rorygc@users.noreply.github.com>
2026-07-03 13:22:52 +0000

    logos: 19 new camp/club/nursery logos + Fresh Arts fix, bump cache

--- logo-overrides.js
+++ logo-overrides.js
@@ -5,21 +5,44 @@ window.WF_CARD_IMAGES = {
 };
 
 window.WF_LOGOS = {
+  // Activities
   "act-canal-cafe-theatre": "https://canalcafetheatre.com/wp-content/uploads/2019/07/cropped-canal-cafe-theatre-logo.png",
   "act-classes": "https://classforkids.io/apple-touch-icon.png",
   "act-lexi-cinema": "https://thelexicinema.co.uk/TheLexiCinema/img/logo.png",
   "act-the-playmap": "https://static.wixstatic.com/media/11062b_1f4617b6e9df40e799fa9620a05da32f%7Emv2.jpg/v1/fit/w_2500,h_1330,al_c/11062b_1f4617b6e9df40e799fa9620a05da32f%7Emv2.jpg",
   "act-vue-finchley-road": "https://www.myvue.com/-/media/vuecinemas/general-imagery/vue-logo-transparent-282x146.png?h=146&iar=0&w=282&rev=eeccbe0cfaf44b949400a52e685e82b1",
+  "act-brent-museum": "https://www.brent.gov.uk/-/media/static/logo.svg",
+  "act-swimtank-willesden": "https://swim-tank.co.uk/wp-content/uploads/2026/04/SwimTank-Logo-Navy-Gold.png",
+
+  // Holiday Camps
   "camp-apf-brent": "https://apfactivitycamps.org/wp-content/uploads/2025/01/WITHOUT-SLOGAN--150x150.png",
-  "camp-club-excel-qpcs": "https://clubexcel.co.uk/wp-content/uploads/2025/12/IMG_Parkwin_Club_Excel-7626-1560x875.jpg",
-  "camp-holiday-academy": "https://holidayacademy.co.uk/wp-content/uploads/2018/03/header_new_home.jpg",
+  "camp-breezy-club": "https://static.wixstatic.com/media/65ae33_6ee4463d4056465d819fc82c606f04eb~mv2.png",
+  "camp-club-excel-qpcs": "https://www.clubexcel.co.uk/wp-content/themes/schoolcms/images/logo.png",
+  "camp-dance-by-the-park": "https://hedanceacademy.co.uk/wp-content/uploads/2024/06/cropped-Helena-elizabeth-1.png",
+  "camp-fresh-arts-queens-park": "https://www.fresharts.co/wp-content/uploads/2025/02/Fresh-Arts_Primary-Wordmark_Clearspace_RGB_FROG-GREEN.png",
+  "camp-holiday-academy": "https://holidayacademy.co.uk/wp-content/uploads/2018/12/Holiday_Academy_logo.svg",
+  "camp-jag-salusbury": "https://a.storyblok.com/f/166376/x/df25d2dde6/footer_logo.svg",
+  "camp-jag-willesden-sports-centre": "https://a.storyblok.com/f/166376/x/df25d2dde6/footer_logo.svg",
+  "camp-little-dance-queens-park": "https://thelittledanceacademy.com/wp-content/uploads/2024/10/1-1-e1727970287174.png",
   "camp-mother-nature-science": "https://www.mnature.co.uk/wp-content/uploads/2025/01/british-science-week-mother-nature-science-purple-circle.jpg",
+  "camp-msport-active": "https://cdn.prod.website-files.com/5d98af396e7b481b1404a004/5d98b2396e7b48f95f04bd24_MSport_Active_logo.svg",
   "camp-nwga-queens-park": "https://nwga.co.uk/wp-content/uploads/2023/09/Untitled-design-69-1.png",
+  "camp-petite-performers-west-hampstead": "https://www.thepetiteperformers.com/images/logo.png",
+  "camp-serving-winners": "https://lirp.cdn-website.com/2b5200c3/dms3rep/multi/opt/SW-Logo-Green-1920w.png",
+  "camp-2-da-stage": "https://images.squarespace-cdn.com/content/v1/57a99d84cd0f6802d2600fe4/1470757237790-ZXJ4P198EYYSAYR01ABP/Logo.jpg",
+
+  // Childcare & Nurseries
   "child-anson-primary": "https://www.ansonprimaryschool.com/_site/images/design/thumbnail.jpg",
   "child-bright-star-kilburn": "https://static.wixstatic.com/media/3e9f09_ef101596ad28490d9faf0d1bbd718558%7Emv2.png/v1/fit/w_2500,h_1330,al_c/3e9f09_ef101596ad28490d9faf0d1bbd718558%7Emv2.png",
   "child-broadhurst": "https://www.broadhurstschool.com/wp-content/themes/schoolcms/images/logo.jpg",
+  "child-brondesbury-nursery": "https://static.wixstatic.com/media/777058_493da4168aed449ea58749ad8be0d3ae~mv2.jpg",
   "child-busy-bees-harlesden": "https://www.busybeeschildcare.co.uk/2021bb/assets/img/bb-logo.png",
   "child-busy-bees-kilburn": "https://www.busybeeschildcare.co.uk/2021bb/assets/img/bb-logo.png",
+  "child-college-green": "https://images-cdn.schooljotter3.com/img/b4139589-4676-4cf9-8c92-76dbd9fc74f2/800x800",
+  "child-dollis-hill": "https://cdn.sanity.io/images/xsx6i96c/nfamilyclub/6ee90f527604a89ad61ae6b5ac29b1c925b4ec9b-425x312.svg",
+  "child-kensal-house": "https://cdn.prod.website-files.com/6696e17bc610c9ba7835bd12/66a01a780a5fae2bc6978bdf_LHN%20Master%20Logotype%20Green.svg",
+  "child-mulberry-house": "https://www.mulberryhouseschool.com/wp-content/uploads/2019/01/logo-dark.png",
   "child-richborough": "https://static.wixstatic.com/media/118ebc_1f307a316f1b474992bfd1c4de9daed9~mv2.jpeg/v1/fill/w_2500,h_1875,al_c/118ebc_1f307a316f1b474992bfd1c4de9daed9~mv2.jpeg",
-  "child-snuggle-bunnies": "https://static.wixstatic.com/media/d2bb60_095b48dc114f493090acf5d86011b121%7Emv2.jpg/v1/fill/w_192%2Ch_192%2Clg_1%2Cusm_0.66_1.00_0.01/d2bb60_095b48dc114f493090acf5d86011b121%7Emv2.jpg"
+  "child-snuggle-bunnies": "https://static.wixstatic.com/media/d2bb60_095b48dc114f493090acf5d86011b121%7Emv2.jpg/v1/fill/w_192%2Ch_192%2Clg_1%2Cusm_0.66_1.00_0.01/d2bb60_095b48dc114f493090acf5d86011b121%7Emv2.jpg",
+  "child-treetops": "https://www.treetopsnursery.co.uk/wp-content/uploads/2020/06/logo.png"
 };
